3a65da0691a887087fe4c36704ff7cf729c1fbcd
Your Name <you@example.com>
2019-03-22 14:28:30 -0700

    initial commit

---
+++ christmas.js
@@ -0,0 +1,14 @@
+const christmas = {
+    day1: "A PARTRIGE IN A PAIR TREE",
+    day2: "",
+    day3: "",
+    day4: "Chubtoad",
+    day5: "",
+    day6: "",
+    day7: "",
+    day8: "Smashbros",
+    day9: "",
+    day10: "Flexbox",
+    day11: "",
+    day12: ""
+   };
