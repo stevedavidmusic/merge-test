aa29e49deb41e1779a8e7f2357c2f13b65e7ba7a
Brenden Kennedy <bckennedy97>
2019-03-22 14:31:11 -0700

    sup

--- christmas.js
+++ christmas.js
@@ -1,9 +1,9 @@
 const christmas = {
     day1: "A PARTRIGE IN A PAIR TREE",
-    day2: "",
-    day3: "",
-    day4: "Chubtoad",
-    day5: "",
+    day2: "HEY",
+    day3: "ITS",
+    day4: "YA",
+    day5: "BOY",
     day6: "",
     day7: "",
     day8: "Smashbros",
