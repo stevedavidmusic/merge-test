b940408922efd950b793bc877eeb99d81f9d1abf
Cobyswan <Cobyswan@yahoo.com>
2019-03-22 14:31:00 -0700

    ULTRA MASTER BRANCH

--- christmas.js
+++ christmas.js
@@ -1,14 +1,14 @@
 const christmas = {
     day1: "A PARTRIGE IN A PAIR TREE",
-    day2: "",
-    day3: "",
+    day2: "Shmoney",
+    day3: "CSS",
     day4: "Chubtoad",
-    day5: "",
+    day5: "BIzzaro",
     day6: "",
     day7: "",
     day8: "Smashbros",
     day9: "",
     day10: "Flexbox",
-    day11: "",
+    day11: "Funktown",
     day12: ""
    };
